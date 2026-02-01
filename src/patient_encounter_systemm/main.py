eba664fa8e684959b339e28f72c13f6ccac98449
underoos14 <varnik45@gmail.com>
2026-02-01 17:06:04 +0530

    code quality improvement

--- src/patient_encounter_systemm/main.py
+++ src/patient_encounter_systemm/main.py
@@ -1,10 +1,11 @@
+from datetime import date
 from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
 from patient_encounter_systemm.database import engine, get_db
 from patient_encounter_systemm.models import models
 from patient_encounter_systemm.schemas import schemas
 from patient_encounter_systemm.services import crud
-from sqlalchemy.orm import Session
-from datetime import date
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Patient Encounter System")
@@ -12,26 +13,31 @@ app = FastAPI(title="Patient Encounter System")
 
 @app.get("/")
 def greet():
+    """Home page placeholder"""
     return "This is a patient encounter system"
 
 
 @app.get("/patients/{patient_id}", response_model=schemas.PatientRead)
 def get_patient(patient_id: int, db: Session = Depends(get_db)):
+    """API Endpoint to get patient details by passing patient id"""
     return crud.get_patient_by_id(db, patient_id)
 
 
 @app.post("/patients", response_model=schemas.PatientCreate, status_code=201)
 def create_patient(payload: schemas.PatientCreate, db: Session = Depends(get_db)):
+    """API Endpoint to create a patient profile in database"""
     return crud.create_patient(db, payload)
 
 
 @app.get("/doctors/{doctor_id}", response_model=schemas.DoctorRead)
 def get_doctor(doctor_id: int, db: Session = Depends(get_db)):
+    """API endpoint to get doctor information by doctor id"""
     return crud.get_doctor_by_id(db, doctor_id)
 
 
 @app.post("/doctors", response_model=schemas.DoctorCreate, status_code=201)
 def create_doctor(payload: schemas.DoctorCreate, db: Session = Depends(get_db)):
+    """API Endpoint to create doctor profile in database"""
     return crud.create_doctor(db, payload)
 
 
@@ -39,9 +45,11 @@ def create_doctor(payload: schemas.DoctorCreate, db: Session = Depends(get_db)):
 def get_appointments(
     apt_date: date, doctor_id: int | None = None, db: Session = Depends(get_db)
 ):
+    """API endpoint to get appointment for a specific date, optionally with doctor id"""
     return crud.get_appointment_with_date(db=db, apt_date=apt_date, doctor_id=doctor_id)
 
 
 @app.get("/health")
 async def health_check():
+    """fastapi health check"""
     return {"status": "UP"}
